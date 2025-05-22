dc8c3b0da79323114746ed5f5189f461c5689ba4
Donald Green <150313464+ZodiacHL@users.noreply.github.com>
2025-05-22 11:14:30 -0700

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -38,35 +38,3 @@ document.addEventListener("DOMContentLoaded", function () {
     };
   });
 });
-
-// Handle form submission via JSON
-const form = document.getElementById('submit');
-form.addEventListener('click', function(e) {
-  e.preventDefault();  // Prevent form from submitting the traditional way
-
-  // Gather form data
-  const formData = {
-    name: document.querySelector('input[name="name"]').value,
-    email: document.querySelector('input[name="email"]').value,
-    message: document.querySelector('textarea[name="message"]').value,
-  };
-
-  console.log(formData);
-
-  // Send the form data as JSON to the backend
-  fetch('/contact', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',  // Set the Content-Type header to application/json
-    },
-    body: JSON.stringify(formData),  // Convert form data to JSON
-  })
-  .then(response => response.text())
-  .then(data => {
-    alert(data);  // Display the response message
-    document.getElementById("contact-form").reset();  // Reset the form after successful submission
-  })
-  .catch(error => {
-    alert('Error: ' + error);  // Show an error if the request fails
-  });
-});
